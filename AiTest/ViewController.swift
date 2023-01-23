8a738d437edc4d9b919d963638fa15151b70317a
orangeshop <rhkeh123@naver.com>
2023-01-23 14:43:56 +0900

    Initial Commit

---
+++ AiTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AiTest
+//
+//  Created by 최영호 on 2023/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
